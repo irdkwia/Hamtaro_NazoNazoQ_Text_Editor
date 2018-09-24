d222f29ca7bc624c047862f32cb6109165ef3c60
irdkwia <Nou_ser@hotmail.fr>
2018-09-24 15:26:27 +0200

    Renaming and reorganizing files

--- Setup.py
+++ Setup.py
@@ -2,13 +2,13 @@ import cx_Freeze
 
 #Code used to freeze python code into an executable.
 
-executables = [cx_Freeze.Executable("Editor.py"), base="Win32GUI"]
+executables = [cx_Freeze.Executable("Editor.py", base = "Win32GUI")]
 
 cx_Freeze.setup(
     name="NZNZQ Editor",
-    version="1.0",
+    version="1.1",
     options={"build_exe": {"includes":["tkinter"],
-                           "include_files":["ModClass.py"]}},
+                           "include_files":["Scripts"]}},
     executables = executables
 
     )
